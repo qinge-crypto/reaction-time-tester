5b91634a3744200c64132d9e56c0403776fc52c3
qinge-crypto <qinge@whitman.edu>
2026-05-19 17:18:26 +0800

    Create reaction_time_tester.h
    Add ESP-IDF header file

---
+++ esp-idf-c-version/reaction_time_tester.h
@@ -0,0 +1,17 @@
+#ifndef REACTION_TIME_TESTER_H
+#define REACTION_TIME_TESTER_H
+
+// GPIO pin assignments
+#define LED_PIN 4
+#define START_BUTTON_PIN 5
+#define REACTION_BUTTON_PIN 6
+
+// Random wait time range in milliseconds
+#define MIN_WAIT_TIME_MS 1000
+#define MAX_WAIT_TIME_MS 5000
+
+// Timing helpers
+#define DEBOUNCE_DELAY_MS 200
+#define LOOP_DELAY_MS 10
+
+#endif
